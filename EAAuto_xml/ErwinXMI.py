ce6e432ab91a6989e07ed222fb07d9ca1b637b46
Iodine <h.olthof@optusnet.com.au>
2016-05-19 13:48:28 +1000

    checkpoint

--- EAAuto_xml/ErwinXMI.py
+++ EAAuto_xml/ErwinXMI.py
@@ -1,41 +1,27 @@
 from lxml import etree
-from tkinter.filedialog import askopenfilename
 
-class ERWIN_xmi:
 
+class ErwinXMI:
     def __init__(self, log_message):
 
         self.log_message = log_message
         self.erwin_package_map = {}
         self.erwin_element_map = {}
         self.erwin_relationship_map = {}
-        self.filename = None
-        self.xpath_class = "/XMI/XMI.content/Model_Management.Model/Foundation.Core.Namespace.ownedElement/Model_Management.Package/Foundation.Core.Namespace.ownedElement/Foundation.Core.Class"
+
+        self.xpath_class = "/XMI/XMI.content/Model_Management.Model/Foundation.Core.Namespace.ownedElement/Foundation.Core.Class"
         self.xpath_model = "/XMI/XMI.content/Model_Management.Model/Foundation.Core.Namespace.ownedElement/Model_Management.Package"
-        self.xpath_relationship = ""
+        self.xpath_relationship = "/XMI/XMI.content/Model_Management.Model/Foundation.Core.Namespace.ownedElement/Foundation.Core.Association"
+        self.xpath_generalization = "/XMI/XMI.content/Model_Management.Model/Foundation.Core.Namespace.ownedElement/Foundation.Core.Generalization"
         self.xpath_diagram = ""
 
-        options = {'defaultextension': '.xml',
-                   'filetypes': (('xml', '.sml'), ('xmi', 'xmi')),
-                   'initialdir': 'C:\\HOME\\var\\projects\\python\\EAAUTO',
-                   'initialfile': 'erwin_export.xml',
-                   'parent': self.master,
-                   'title': 'Open ERWIN xmi export'}
-        filename = askopenfilename(**options)
-        if filename:
-            self.build_erwin_map()
-        else:
-            self.log_message("error getting the ERWIN xmi export")
-            return
-
-
-    def build_erwin_map(self):
+    def build_erwin_map(self, filename):
         """build dictionaries of ERWIN elements, packages and relationships:
             key = ERWIN GUID
             value = name
             """
         self.log_message("starting erwin xmi mapping")
-        doc = etree.parse(self.filename)
+        doc = etree.parse(filename)
         r = doc.getroot()
         # collect and process all Model - aka packages
         rpath = doc.xpath(self.xpath_model)
